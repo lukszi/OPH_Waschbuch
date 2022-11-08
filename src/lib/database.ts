c0e18d9a618bd7cd7d2c7883cc3153cb57c19fd6
Lukas Szimtenings <lszimtenings@ukaachen.de>
2022-11-08 06:52:33 +0100

    refactor DB URI

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -15,14 +15,14 @@ const appointmentSchema = new Schema<IAppointment>({
     timeSlot: {type: Number, required: true},
     date: { type: Date, required: true }
 });
-
 const AppointmentModel = model<IAppointment>('Appointment', appointmentSchema);
+const dbUri = 'mongodb://localhost:27017/washing_book';
 
 /**
  * Get all appointments for a given date
  */
 export async function findByDate(date: Date): Promise<IAppointment[]>{
-    await connect('mongodb://localhost:27017/washing_book');
+    await connect(dbUri);
     return AppointmentModel.find({date: date});
 }
 
@@ -34,7 +34,7 @@ export async function insert(appointment: Appointment){
     if (appointment.user === null){
         throw error(400, "User cannot be null");
     }
-    await connect('mongodb://localhost:27017/washing_book');
+    await connect(dbUri);
     const dbAppointment = new AppointmentModel({
         machine: appointment.machine.name,
         user: {room: appointment.user.room, name: appointment.user.name},
@@ -47,7 +47,7 @@ export async function insert(appointment: Appointment){
 
 export async function findByDateSlotAndMachine(date: Date, timeSlot: number, machine: string): Promise<IAppointment | null>{
     console.log("findByDateSlotAndMachine", date, timeSlot, machine);
-    await connect('mongodb://localhost:27017/washing_book');
+    await connect(dbUri);
     return AppointmentModel.findOne({date: date, timeSlot: timeSlot, machine: machine});
 }
 
@@ -55,7 +55,7 @@ export async function deleteAppointment(appointment: Appointment){
     if (appointment.user === null){
         throw error(400, "User cannot be null");
     }
-    await connect('mongodb://localhost:27017/washing_book');
+    await connect(dbUri);
     const filter = {
         machine: appointment.machine.name,
         "user.name": appointment.user.name,
