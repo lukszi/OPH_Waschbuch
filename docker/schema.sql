20934c4b53e46bcc56b915868afe33dffb82aa49
Lukas Szimtenings <lszimtenings@ukaachen.de>
2022-11-28 12:52:56 +0100

    Initialize db with default data

--- docker/schema.sql
+++ docker/schema.sql
@@ -1,8 +1,7 @@
 CREATE DATABASE washbook;
-CREATE USER wbAdmin WITH PASSWORD 'washPw';
-GRANT ALL ON DATABASE washbook TO wbAdmin;
 \connect washbook;
 
+-- Create Tables --
 CREATE TABLE Users (
     id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name varchar(255) NOT NULL,
@@ -32,5 +31,17 @@ CREATE TABLE Appointment (
     FOREIGN KEY (slot_id) REFERENCES Slot(id)
 );
 
+-- Create washbook user --
+CREATE USER wbAdmin WITH PASSWORD 'washPw';
+GRANT ALL ON DATABASE washbook TO wbAdmin;
 GRANT ALL ON SCHEMA public TO wbAdmin;
 GRANT ALL ON ALL TABLES IN SCHEMA public TO wbAdmin;
+
+-- Initialize washers --
+INSERT INTO Washer (name) VALUES ('Machine 1'), ('Machine 2');
+-- Initialize slots --
+INSERT INTO Slot (start_time, end_time) VALUES
+                                            ('00:00:00', '02:00:00'), ('02:00:00', '04:00:00'), ('04:00:00', '06:00:00'),
+                                            ('06:00:00', '08:00:00'), ('08:00:00', '10:00:00'), ('10:00:00', '12:00:00'),
+                                            ('12:00:00', '14:00:00'), ('14:00:00', '16:00:00'), ('16:00:00', '18:00:00'),
+                                            ('18:00:00', '20:00:00'), ('20:00:00', '22:00:00'), ('22:00:00', '00:00:00');
